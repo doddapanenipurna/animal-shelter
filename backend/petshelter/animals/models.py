842d1dbe6d253c083cd6551b2f0c2875686819b9
Purna <doddapanenipc@tamu.edu>
2021-03-15 15:08:28 -0500

    Make migrations and add null attributes to Animal model

--- backend/petshelter/animals/models.py
+++ backend/petshelter/animals/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 
@@ -18,19 +18,28 @@ class Animal(models.Model):
         (OTHER, 'Other')
     ]
 
-    shelter_id = models.CharField(default=None, blank=True, null=True)
-    intake_date = models.DateTimeField(default=datetime.now)
-    species = models.CharField(default=None, blank=True)
-    breed = models.CharField(default=None, blank=True)
-    gender = models.CharField(default=None, blank=True, choices=ANIMAL_GENDERS)
-    age_years = models.IntegerField(default=None, blank=True)
-    age_months = models.IntegerField(default=None, blank=True)
-    age_weeks = models.IntegerField(default=None, blank=True)
-    age_days = models.IntegerField(default=None, blank=True)
-    # do some calcuations here to deteremine it from the age parameters provided
-    dob_estimate = models.DateTimeField(deafult=None)
-    location = models.CharField(default="Shelter", blank=True, null=True)
+    # Allow for all fields to be blank. Shelter may not always input each and every piece of information
+    # at time of creation.
+    shelter_id = models.CharField(
+        max_length=255, default=None, blank=True, null=True)
+    intake_date = models.DateTimeField(auto_now_add=True)
+    species = models.CharField(
+        max_length=255, default=None, blank=True, null=True)
+    breed = models.CharField(
+        max_length=255, default=None, blank=True, null=True)
+    gender = models.CharField(
+        max_length=255, default=None, blank=True, null=True, choices=ANIMAL_GENDERS)
+    age_years = models.IntegerField(default=None, blank=True, null=True)
+    age_months = models.IntegerField(default=None, blank=True, null=True)
+    age_weeks = models.IntegerField(default=None, blank=True, null=True)
+    age_days = models.IntegerField(default=None, blank=True, null=True)
+    dob_estimate = models.DateTimeField(default=None, null=True)
+    location = models.CharField(
+        max_length=255, default="Shelter", blank=True, null=True)
     fee = models.IntegerField(blank=True, null=True)
     weight = models.IntegerField(default=None, blank=True, null=True)
-    color = models.CharField(default=None, blank=True, null=True)
-    notes = models.TextField(default=None, blank=True,null=True )
+    color = models.CharField(
+        max_length=255, default=None, blank=True, null=True)
+    notes = models.TextField(default=None, blank=True, null=True)
+    name = models.CharField(
+        max_length=255, default=None, blank=True, null=True)
--- backend/petshelter/animals/views.py
+++ backend/petshelter/animals/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from .models import Animal
 def index(request):
     return HttpResponse("JoJo is barking")
+
+def add(request):
     
+    return HttpResponse("Animal Added")
